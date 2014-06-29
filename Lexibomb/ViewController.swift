1e2319ac5164b33e19e1b78137832c2b98910419
Alan Westbrook <alan@rockwoodsoftware.com>
2014-06-29 13:45:21 -0700

    Skeleton in Swift

--- Lexibomb/ViewController.swift
+++ Lexibomb/ViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UICollectionViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var tiles: String[]
+    
+    init(coder aDecoder: NSCoder!)  {
+        tiles = ["one", "two", "three", "four", "5", "6", "7"]
+        
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell! {
+        
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as UICollectionViewCell
+        
+        var label = cell.viewWithTag(1001) as UILabel
+        
+        label.text = self.tiles[indexPath.row]
+        
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int {
+        return self.tiles.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView!, didSelectItemAtIndexPath indexPath: NSIndexPath!) {
+        NSLog("Pressed %@", self.tiles[indexPath.row])
+    }
 }
 
