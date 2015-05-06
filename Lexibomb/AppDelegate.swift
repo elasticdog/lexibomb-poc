1adea56658eeb72625339b520234598466097f5b
Alan Westbrook <alan@rockwoodsoftware.com>
2015-05-06 10:58:13 -0700

    No phonses
    * Change the project to only be valid for pads
    * Update the sdks to 8.3
    * add an assert to the code checking for pad device

--- Lexibomb/AppDelegate.swift
+++ Lexibomb/AppDelegate.swift
@@ -13,6 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                             
     var window: UIWindow?
 
+    func application(application: UIApplication, willFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        assert(UIDevice.currentDevice().userInterfaceIdiom == .Pad, "Lexibom(tm)(sm)(r)(c) can only run on pad-like devices")
+        return true
+    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
